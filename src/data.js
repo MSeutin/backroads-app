2f0490d0c06b1a7ca9908be7abb64b56a3e33e14
Michael Seutin <frenchmike@gmail.com>
2023-06-02 22:13:16 -0700

    first

---
+++ src/data.js
@@ -0,0 +1,19 @@
+export const pageLinks = [
+    { id: 1, href: '#home', text: 'home' },
+    { id: 2, hred: '#about', text: 'about' },
+    { id: 3, href: '#services', text: 'services' },
+    { id: 4, href: '#tours', text: 'tours' },
+
+];
+
+export const socialLinks = [
+    { id: 1, href: 'https://www.twitter.com', icon: 'fab fa-twitter' },
+    { id: 2, href: 'https://www.twitter.com', icon: 'fab fa-facebook' },
+    { id: 3, href: 'https://www.twitter.com', icon: 'fab fa-squarespace' },
+]
+
+export const services = [
+    { id: 1, icon: 'fas fa-map-marker-alt', title: 'saving money', text: 'lorem ipsum once upon a time' },
+    { id: 2, icon: 'fas fa-tree', title: 'endless hiking', text: 'lorem ipsum once upon a time' },
+    { id: 3, icon: 'fas fa-socks', title: 'amazing comfort', text: 'lorem ipsum once upon a time' },
+]
